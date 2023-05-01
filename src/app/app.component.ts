a41c29c1859064d3df2f377d72e144e73e234215
BuyakovaVA <lineviktoriya@gmail.com>
2023-05-01 19:33:02 +0200

    add filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   title = 'angular-shop';
   loading: boolean = false;
   products$: Observable<IProduct[]>;
+  filter: string = '';
 
   ngOnInit(): void {
     this.loading = true;
