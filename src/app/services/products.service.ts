2ca7f9685238cd85682ed88e923e8385a58c6a4c
BuyakovaVA <lineviktoriya@gmail.com>
2023-05-21 22:47:58 +0200

    add cart methods

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -20,6 +20,47 @@ export class ProductsService {
     );
   }
 
+  saveCart(cart: IProduct[]){
+    return localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  addToCart(product: IProduct) {
+    let cart: IProduct[] = [];
+    if (localStorage.getItem('cart') !== null) {
+      cart = JSON.parse(localStorage.getItem('cart') || '{}');
+    }
+    cart.push(product);
+    this.saveCart(cart);
+  }
+
+  loadCart(): IProduct[] {
+    if (localStorage.getItem('cart') !== null) {
+      return JSON.parse(localStorage.getItem('cart') || '{}');
+    }
+    return [];
+  }
+
+  productInCart(product: IProduct): boolean {
+    let cart: IProduct[] = [];
+    if (localStorage.getItem('cart') !== null) {
+      cart = JSON.parse(localStorage.getItem('cart') || '{}');
+    }
+    return cart.some(p => p.id === product.id);
+  }
+
+  removeProductFromCart(product: IProduct) {
+    let cart: IProduct[] = [];
+    if (localStorage.getItem('cart') !== null) {
+      cart = JSON.parse(localStorage.getItem('cart') || '{}');
+    }
+    cart = cart.filter(p => p.id !== product.id);
+    this.saveCart(cart);
+  }
+
+  clearCart() {
+    localStorage.removeItem('cart');
+  }
+
   private errorHandler(error: HttpErrorResponse) {
     this.errorService.handleError(error.message);
     return throwError(() => error.message);
